934fb4ccc7749caf7685e5419d467f295502e660
Riekus <riekusgroblerps4@gmail.com>
2026-06-14 21:18:45 +0200

    Begining

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = 5000 || process.env.PORT
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
