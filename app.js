2889c65da7a3775e5f6ad68c3bbb8adafb75ce71
Riekus <riekusgroblerps4@gmail.com>
2026-06-15 16:29:13 +0200

    new aditions

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 require('dotenv').config();
 
+// express layout is just extra 
+// step 1: require the express module
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+//step 2: use express by creating an app variable 
 const app = express();
 const PORT = 5000 || process.env.PORT
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-});
+// setup a static folder we can make changes in, this also allows us to split our logic
+app.use(express.static('public'));
+
+// this is extra but also an example of middleware
+//middleware 
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// this is an example of using express directly as oppose of using routers (we'll use that later)
+// app.get("/", (req, res) => {
+//     res.send("Hello world");
+// });
+
+app.use("/", require('./server/routes/main'));
+
+// step 3: always listen on port x
+app.listen(PORT, () => {
+    console.log(`App active on port ${PORT}`);
+})
---
+++ server/routes/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// now we split our app into a router that does the routing
+const router = express.Router();
+
+//Routes
+router.get('', (req, res) => {
+    const locals = {
+        title: "Node JS Blog",
+        description: "Simple blog using Node JS, EJS and Mongo DB :)"
+    }
+    res.render('index', {locals});
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+//now we export our router module so the app should work
+module.exports = router
